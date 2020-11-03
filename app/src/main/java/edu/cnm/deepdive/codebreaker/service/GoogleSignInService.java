2c2e76d2d5022b6e65aebc77796d3b94ce407430
AugmenTab <tbaum92@live.com>
2020-11-03 09:19:41 -0700

    Started back-end integration.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/service/GoogleSignInService.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/service/GoogleSignInService.java
@@ -3,12 +3,14 @@ package edu.cnm.deepdive.codebreaker.service;
 import android.app.Activity;
 import android.app.Application;
 import android.content.Intent;
+import android.util.Log;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+import edu.cnm.deepdive.codebreaker.BuildConfig;
 
 public class GoogleSignInService {
 
@@ -20,9 +22,7 @@ public class GoogleSignInService {
 
   private GoogleSignInService() {
     GoogleSignInOptions options = new GoogleSignInOptions.Builder()
-        .requestEmail().requestId().requestProfile()
-//        .requestIdToken(BuildConfig.CLIENT_ID)
-        .build();
+        .requestEmail().requestId().requestProfile().requestIdToken(BuildConfig.CLIENT_ID).build();
     client = GoogleSignIn.getClient(context, options);
   }
 
@@ -39,7 +39,7 @@ public class GoogleSignInService {
   }
 
   public Task<GoogleSignInAccount> refresh() {
-    return client.silentSignIn().addOnSuccessListener((account) -> this.account = account);
+    return client.silentSignIn().addOnSuccessListener(this::setAccount);
   }
 
   public void startSignIn(Activity activity, int requestCode) {
@@ -52,7 +52,7 @@ public class GoogleSignInService {
     Task<GoogleSignInAccount> task = null;
     try {
       task = GoogleSignIn.getSignedInAccountFromIntent(data);
-      account = task.getResult(ApiException.class);
+      setAccount(task.getResult(ApiException.class));
     } catch (ApiException e) {
       // Exception will be passed automatically to onFailureListener.
     }
@@ -60,7 +60,15 @@ public class GoogleSignInService {
   }
 
   public Task<Void> signOut() {
-    return client.signOut().addOnCompleteListener((ignored) -> account = null);
+    return client.signOut().addOnCompleteListener((ignored) -> setAccount(null));
+  }
+
+  private void setAccount(GoogleSignInAccount account) {
+    this.account = account;
+    if (account != null) {
+      //noinspection ConstantConditions
+      Log.d(getClass().getSimpleName(), account.getIdToken());
+    }
   }
 
   private static class InstanceHolder {
