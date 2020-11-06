2164129daff4f2a7dda460285450bf7d7b3c2bd9
AugmenTab <tbaum92@live.com>
2020-11-05 17:00:47 -0700

    Verified round trip on user profile request.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/model/dao/UserDao.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/model/dao/UserDao.java
@@ -1,19 +1,22 @@
 package edu.cnm.deepdive.codebreaker.model.dao;
 
 import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
+import androidx.room.Update;
 import edu.cnm.deepdive.codebreaker.model.entity.User;
-import io.reactivex.Maybe;
 import io.reactivex.Single;
 
+@Dao
 public interface UserDao {
 
-  @Insert
+  @Insert(onConflict = OnConflictStrategy.IGNORE)
   Single<Long> insert(User user);
 
-  @Insert
+  @Update
   Single<Integer> update(User user);
 
   @Delete
@@ -23,8 +26,6 @@ public interface UserDao {
   LiveData<User> selectById(long userId);
 
   @Query("SELECT * FROM user_profile WHERE oauth_key = :oauthKey")
-  Maybe<User> selectByOauthKey(String oauthKey);
-
-
+  Single<User> selectByOauthKey(String oauthKey);
 
 }
